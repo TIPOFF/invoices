096e6d1c533594855e0ca69164e9711eae170771
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-02-05 19:30:25 -0500

    Add invoice policy

---
+++ src/AuthServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tipoff\Invoices;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Tipoff\Invoices\Models\Invoice;
+use Tipoff\Invoices\Policies\InvoicePolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Invoice::class => InvoicePolicy::class,
+    ];
+
+    /**
+     * Register the application's policies.
+     *
+     * @return void
+     */
+    public function registerPolicies()
+    {
+        foreach ($this->policies() as $key => $value) {
+            Gate::policy($key, $value);
+        }
+    }
+
+    /**
+     * Get the policies defined on the provider.
+     *
+     * @return array
+     */
+    public function policies()
+    {
+        return $this->policies;
+    }
+}
