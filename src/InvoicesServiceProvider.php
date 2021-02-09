d4dcc5a2980f58caee31d57d5a4f6c90347596a4
WebTigers <WebTigers@users.noreply.github.com>
2021-02-09 23:23:34 +0000

    Fix styling

--- src/InvoicesServiceProvider.php
+++ src/InvoicesServiceProvider.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Tipoff\Invoices;
 
-use Illuminate\Support\Str;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Tipoff\Invoices\Commands\InvoicesCommand;
 
 class InvoicesServiceProvider extends PackageServiceProvider
 {
